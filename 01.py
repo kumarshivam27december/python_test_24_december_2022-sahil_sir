43e9ee1918581bc3119c4db18a5f43e637ef9c17
Shivam Kumar <kumarshivam16062004@gmail.com>
2022-12-24 10:42:54 +0530

    version 2.0

--- 01.py
+++ 01.py
@@ -39,3 +39,104 @@ print me.getClassiness()
 me.addItem("bowtie")
 # Should be 15
 print me.getClassiness()'''
+
+
+
+
+
+# class Classy(object):
+#     def _init_(self):
+#         self.items = []
+ 
+#     def addItem(self, str1):
+#         self.items.append(str1)
+ 
+#     def getClassiness(self):
+#         sum = 0
+#         for item in self.items:
+#             if (item == "tophat"):
+#                 sum = sum + 2
+#             elif (item == "bowtie"):
+#                 sum = sum + 4
+#             elif (item == "monocle"):
+#                 sum = sum + 5
+#         return sum
+ 
+# # Test cases
+# me = Classy()
+ 
+# # Should be 0
+# print me.getClassiness()
+ 
+# me.addItem("tophat")
+# # Should be 2
+# print me.getClassiness()
+ 
+# me.addItem("bowtie")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Classy(object):
+    def __init__(self):
+        self.items = []
+        self.classiness = 0
+
+    def getClassiness(self):
+        try:
+            for i in self.items:
+                if i == "tophat":
+                    self.classiness += 2
+                elif i == "bowtie":
+                    self.classiness += 4
+                elif i == "monocle":
+                    self.classiness += 5
+                self.items.pop(0)
+            self.items.pop(0)
+        except IndexError as e:
+            pass
+        return self.classiness
+
+    def addItem(self, item):
+        self.items.append(item)
+
+# Test cases
+me = Classy()
+
+# Should be 0
+print (me.getClassiness())
+
+me.addItem("tophat")
+# Should be 2
+print (me.getClassiness())
+
+me.addItem("bowtie")
+me.addItem("jacket")
+me.addItem("monocle")
+# Should be 11
+print (me.getClassiness())
+
+me.addItem("bowtie")
+# Should be 15
+print (me.getClassiness())
